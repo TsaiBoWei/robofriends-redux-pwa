30e3f8bfbd56a9e23f33883f3016f506209c0c55
Tsai Bo Wei(Brian) <eliot9010@gmail.com>
2020-07-02 15:31:28 +0800

    add build

---
+++ build/precache-manifest.466c3ff5452e6e367b204a562a606b52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98c320a785ae1da2e7f163745dbd7fc",
+    "url": "/robofriends-redux-pwa/index.html"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/robofriends-redux-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "09dc69b74163da4c7042",
+    "url": "/robofriends-redux-pwa/static/css/main.eac8b138.chunk.css"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/robofriends-redux-pwa/static/js/2.8bf57d0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends-redux-pwa/static/js/2.8bf57d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09dc69b74163da4c7042",
+    "url": "/robofriends-redux-pwa/static/js/main.0383ca95.chunk.js"
+  },
+  {
+    "revision": "442cb5f60b454540655e",
+    "url": "/robofriends-redux-pwa/static/js/runtime-main.0706ef70.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
